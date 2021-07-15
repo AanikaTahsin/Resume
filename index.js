4417bb168e3cd9d025842b227c36866d794e3002
CLown1331 <clown1331@gmail.com>
2021-07-15 19:31:31 +0600

    Update

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ const shellExec = (command, errorMessage, exitCode, errorCode) => {
     }
 }
 
+shell.rm('-R', ['./public']);
+shell.mkdir('public');
 shellExec('pdflatex main', 'pdf creation failed', 0, 1);
 shell.mv('./main.pdf', './Araf\ Al-Jami.pdf');
+shell.cp('./Araf\ Al-Jami.pdf', './public/Araf\ Al-Jami.pdf');
