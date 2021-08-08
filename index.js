4776291c23cde339278b0e50f808db6f5a79b4fc
CLown1331 <clown1331@gmail.com>
2021-08-09 02:39:29 +0600

    Update

--- index.js
+++ index.js
@@ -22,10 +22,24 @@ const html =
     </body>
 </html>`;
 
+const resumeHtml = 
+`<html>
+    <head>
+        <script>
+        </script>
+    </head>
+    <body>
+        <div style="margin: 0 auto; max-width: 55rem;">
+        ${fs.readFileSync('./resume.html')}
+        </div>
+    </body>
+</html>`
+
 shell.rm('-R', ['./public']);
 shell.mkdir('public');
 shellExec('pdflatex main', 'pdf creation failed', 0, 1);
 fs.writeFileSync('./public/index.html', html);
+fs.writeFileSync('./public/resume.html', resumeHtml);
 shell.mv('./main.pdf', './Araf\ Al-Jami.pdf');
 shell.cp('./Araf\ Al-Jami.pdf', './public/Araf\ Al-Jami.pdf');
-shell.cp('./resume.html', './public/resume.html');
+// shell.cp('./resume.html', './public/resume.html');
