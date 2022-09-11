43790295c2337659ebcf5086e295fc1cb0263b49
Araf Al Jami <clown1331@gmail.com>
2022-09-11 23:55:19 +0700

    Update deps

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const shell = require('shelljs');
 const fs = require('fs');
+const puppeteer = require('puppeteer');
 
 const shellExec = (command, errorMessage, exitCode, errorCode) => {
     if (shell.exec(command).code != exitCode) {
@@ -22,11 +23,29 @@ const html =
     </body>
 </html>`;
 
-shell.rm('-R', ['./public']);
-shell.mkdir('public');
-shellExec('resume export resume.pdf --format pdf --theme jsonresume-theme-straightforward', 'pdf creation failed', 0, 1);
-shellExec('resume export resume.html --format html --theme jsonresume-theme-straightforward', 'pdf creation failed', 0, 1);
-fs.writeFileSync('./public/index.html', html);
-shell.mv('./resume.pdf', './Araf\ Al-Jami.pdf');
-shell.cp('./Araf\ Al-Jami.pdf', './public/Araf\ Al-Jami.pdf');
-shell.cp('./resume.html', './public/resume.html');
+(async () => {
+    shell.rm('-R', ['./public']);
+    shell.mkdir('public');
+    shellExec('resume export resume.html --format html --theme jsonresume-theme-straightforward', 'pdf creation failed', 0, 1);
+    fs.writeFileSync('./public/index.html', html);
+
+    const browser = await puppeteer.launch({
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
+    });
+
+    const page = await browser.newPage();
+
+    await page.setContent(fs.readFileSync('./resume.html', 'utf-8'));
+
+    await page.pdf({
+      path: 'resume.pdf',
+      format: 'A4',
+      printBackground: true,
+    });
+
+    await browser.close();
+    
+    shell.mv('./resume.pdf', './Araf\ Al-Jami.pdf');
+    shell.cp('./Araf\ Al-Jami.pdf', './public/Araf\ Al-Jami.pdf');
+    shell.cp('./resume.html', './public/resume.html');
+})();
